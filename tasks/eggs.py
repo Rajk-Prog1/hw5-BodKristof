3032fe6d64d0712d7673aedfdbfa702b4da18c3b
BodKristof <113345795+BodKristof@users.noreply.github.com>
2024-04-03 15:44:41 +0000

    eggs 1

--- tasks/eggs.py
+++ tasks/eggs.py
@@ -1,5 +1,13 @@
 def eggs_solution(breaks):
-    for i in range(1,101):
-        if breaks(i):
-            return i - 1
-    return 100
+    lepes=14
+    szamolo=1
+
+    for i in range(0,14):
+        szamolo=szamolo+lepes
+        if breaks(szamolo):
+            for j in range(szamolo-lepes,szamolo):
+                if breaks(j):
+                    return j - 1
+        else:
+            lepes=lepes-1
+
